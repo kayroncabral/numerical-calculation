171c0d2855720c8a6cbd30e1619ce7c00621759e
Kayron Cabral <kayron7@gmail.com>
2016-05-13 09:19:13 -0400

    adjusting script comments

--- linearfit.py
+++ linearfit.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# matrix triangulating
 def triangulating(matrix):
 	lines = matrix.shape[0]
 	columns = matrix.shape[1]
 
-	# Tranforma o pivo em 1 caso ele ainda nao seja
+	# Turns the pivot into one
 	if matrix[0,0] != 1:
 		for line in range(1):
 			head = matrix[line,0]
@@ -13,14 +14,14 @@ def triangulating(matrix):
 				if line == 0:
 					matrix[line, colum] = matrix[line, colum] / head
 	
-	# transforma a matriz em triangular superior 
+	# Turns matrix in upper triangular
 	pivot = matrix[0,0]
 	for line in range(1, lines):
 		head = matrix[line,0]
 		for colum in range(columns):
 			matrix[line, colum] = (head * matrix[line - 1, colum]) - (matrix[line, colum] * pivot)
 
-	# transforma a diagonal principal em 1
+	# turns main diagonal into one
 	for line in range(1, lines):
 		pivot = matrix[line, line]
 		for colum in range(columns):
@@ -28,9 +29,11 @@ def triangulating(matrix):
 
 	return matrix
 
-def buildMatrix(x, y):
+# Builds a linear system in the format
 	# aEx 	+ 	bn 		= Ey
 	# aEx^2 + 	bEx 	= Exy
+# from two lists
+def buildMatrix(x, y):
 	sumX = 0.0
 	sumY = 0.0
 	sumX2 = 0.0
@@ -42,19 +45,20 @@ def buildMatrix(x, y):
 		sumX2 += pow(x[index], 2)
 	return np.matrix([[sumX, len(x), sumY], [sumX2, sumX, sumXY]])
 
-
+# find the matrix coefficients
 def coefficients(matrix):
 	b = matrix[matrix.shape[0] - 1, matrix.shape[1] - 1]
 	a = (matrix[matrix.shape[0] - 2, matrix.shape[1] - 1]) - (b * matrix[matrix.shape[0] - 2, matrix.shape[1] - 2])	
 	return (a, b)
 
+# creates the y axis linear adjustment
 def yAxis(xAxix, coeff):
 	array = []
 	for index in range(len(xAxix)):
 		array.append(coeff[0] * xAxix[index] + coeff[1])
 	return array
 
-
+# plot the graph
 def plot(x, y, y2, coeff):
 	plt.plot(x, y, 'ro', x, y2, 'b--')
 	plt.title('Ajuste Linear\ny = {}x + {}'.format(coeff[0], coeff[1]))
