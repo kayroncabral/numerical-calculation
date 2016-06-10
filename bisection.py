29273f7cd93272825cfe0574e07c9a9306b75fa2
Kayron Cabral <kayron.scabral@gmail.com>
2016-06-10 09:42:07 -0400

    read params from argv

--- bisection.py
+++ bisection.py
@@ -1,19 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import *
+import sys
 
 # Creates X axis
-def xAxis():
-	return np.arange(-1, 2, 0.01)
+def xAxis(high, lower, interval):
+	h = (high - lower) / interval
+	return np.arange(array[0], array[1], h)
 
 # Creates y axis
-def yAxis(xAxis):
+def yAxis(xAxis, function):
 	array = []
-	for value in xAxis:
-		array.append(np.cos(np.pi*value))
+	for x in xAxis:
+		array.append(eval(function))
 	return array
 
 # Returns de midpoint
-def bisection(a, b, iterations):
+def bisection(y, a, b, iterations):
 	count = 0
 	while count < iterations:
 		xm = (a + b) / 2
@@ -25,24 +28,31 @@ def bisection(a, b, iterations):
 	return xm
 
 # plot the graph
-def plot(x, y, xm):
-	plt.plot(x, y, 'r--', x[xm], 0, 'ro')
-	plt.title('Bisection Method\ncos(x)')
+def plot(x, y, xm, function):
+	plt.plot(x, y, 'r--', x[xm], y[xm], 'ro')
+	plt.title('Bisection Method\nf(x) = {}; xm = {}'. format(function, y[xm]))
 	plt.xlabel('x')
 	plt.ylabel('y')
 	plt.grid(True)
 	plt.show()
 
+def main(argv):
+	if len(argv) == 3:
+		iterations = int(argv[0])
+        range = np.fromstring(argv[1], dtype = float, sep = ',')
+        x = xAxis(, iterations)
+		function = argv[2]
+		y = yAxis(x, function)
+
+		a = 0
+		b = len(x) - 1
+
+		xm = bisection(y, a, b, iterations)
+
+		plot(x, y, xm, function)
+	else:
+		print("{} <iteration> <Xmin,Xmax> <function>".format(sys.argv[0]))
+
 # Only run if a scritp
 if __name__ == "__main__":
-
-	iterations = 100
-	x = xAxis()
-	y = yAxis(x)
-
-	a = 0
-	b = len(x) - 1
-
-	xm = bisection(a, b, iterations)
-
-	plot(x, y, xm)
+	main(sys.argv[1:])
--- trapezoidal.py
+++ trapezoidal.py
@@ -1,23 +1,21 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from math import *
+import sys
 
 # Calculates height
 def height(high, lower, interval):
 	return (high - lower) / interval
 
 # Creates X axis
-def xAxis(lower, high):
-	array = []
-	value = lower
-	while value <= high:
-		array.append(value)
-		value += h
-	return array
+def xAxis(high, lower, height):
+	return np.arange(lower, high, height)
 
 # Creates y axis
-def yAxis(xAxis):
+def yAxis(xAxis, function):
 	array = []
-	for index in range(len(xAxis)):
-		array.append(pow(xAxis[index], 2))
+	for x in xAxis:
+		array.append(eval(function))
 	return array
 
 # Calculates the total area
@@ -28,7 +26,7 @@ def area(yAxis, h):
 	return area
 
 # plot the graph
-def plot(xAxis, yAxis, interval, h):
+def plot(xAxis, yAxis, function, interval, h):
 	plt.plot(xAxis, yAxis, 'ro', xAxis, yAxis, 'b')
 
 	# Draw vertical and diagonal lines
@@ -36,20 +34,26 @@ def plot(xAxis, yAxis, interval, h):
 		plt.plot([xAxis[index - 1], xAxis[index]], [yAxis[index - 1], yAxis[index]], 'r--')
 		plt.plot([xAxis[index], xAxis[index]], [0, yAxis[index]], 'r--')
 
-	plt.title('Regra do Trapezio\nIntervalos: {} e Area total: {}'.format(interval, area(yAxis, h)))
+	plt.title('Bisection Method\nf(x) = {}; intervalos = {}; area total = {}'.format(function, interval, area(yAxis, h)))
 	plt.xlabel('x')
 	plt.ylabel('y')
 	plt.grid(True)
 	plt.show()
 
+def main(argv):
+	if len(argv) == 3:
+		interval = int(argv[0])
+		range = np.fromstring(argv[1], dtype = float, sep = ',')
+		lower = range[0]
+		high = range[1]
+		function = argv[2]
+
+		h = height(high, lower, interval)
+		x = xAxis(high,lower, h)
+		y = yAxis(x, function)
+		plot(x, y, function, interval, h)
+	else:
+		print("{} <iteration> <Xmin,Xmax> <function>".format(sys.argv[0]))
 # Only run if a scritp
 if __name__ == "__main__":
-
-	lower = 0.0
-	high = 3.0
-	interval = 10
-
-	h = height(high, lower, interval)
-	x = xAxis(lower, high)
-	y = yAxis(x)
-	plot(x, y, interval, h)
+	main(sys.argv[1:])
